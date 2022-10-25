b634ed220619fd2583970d3f88ff59717da49606
Nayem <naeemislam13790@gmail.com>
2022-10-25 13:21:25 +0600

    'commited'

--- src/src/settings.py
+++ src/src/settings.py
@@ -98,11 +98,21 @@ ASGI_APPLICATION = 'src.asgi.application'
 
 
 
+
+
+
+
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [("127.0.0.1", 6379)],
+                
+            },
+        },
     }
-}
+
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
