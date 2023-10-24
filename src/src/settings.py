1438c1e014e2c0678918c147bdd88a31bbf76ed2
Nayem <nayemmiah.cu@gamil.com>
2023-10-25 00:11:46 +0600

    'commited'

--- src/src/settings.py
+++ src/src/settings.py
@@ -174,6 +174,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
